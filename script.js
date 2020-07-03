3c380e19403bfa5d321f714c40d1e5555dd23d01
“Muhammad Aldien Said” <aldien03@gmail.com>
2020-07-03 15:25:25 +0700

    working seismic-o-meter

--- script.js
+++ script.js
@@ -6,36 +6,43 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 
-// Test prompt
-// const userCXdate = prompt("When will be our crew change date?");
-// console.log(userCXdate);
-
-// const setCXdate = document.getElementById(setCXdate).onclick;
+// Function Set Date on Click
 
 // Set crew change dates //Hard-code test
-const cxDate = new Date("August 01 2020 00:00:00");
+// var cxDate = new Date("August 01 2020 00:00:00");
+let cxDate = new Date();
+let today = new Date();
+
+document.getElementById("btnDepartureDate").onclick = function () {
+  let setTargetDate = document.getElementById("departureDate").value;
+  cxDate = new Date(setTargetDate);
+};
 
 // Function to check the remaining days etc
 
 function remainingCX() {
-  let today = new Date();
-  //   console.log(today);
-  let timeToCX = cxDate - today;
-  //   console.log(remainingDay); => Harus convert dari seconds
+  if (today === cxDate) {
+    days.innerHTML = 00;
+    hours.innerHTML = 00;
+    minutes.innerHTML = 00;
+    seconds.innerHTML = 00;
+  } else {
+    let timeToCX = cxDate - today;
+    //   console.log(remainingDay); => Harus convert dari seconds
+    let remainingDay = Math.floor(timeToCX / 1000 / 60 / 60 / 24);
+    //   console.log(remainingDay);
+    let remainingHour = Math.floor(timeToCX / 1000 / 60 / 60) % 24;
+    //   console.log(remainingHour);
+    let remainingMinute = Math.floor(timeToCX / 1000 / 60) % 60;
+    //   console.log(remainingMinute);
+    let remainingSecond = Math.floor(timeToCX / 1000) % 60;
+    //   console.log(remainingSecond);
 
-  let remainingDay = Math.floor(timeToCX / 1000 / 60 / 60 / 24);
-  //   console.log(remainingDay);
-  let remainingHour = Math.floor(timeToCX / 1000 / 60 / 60) % 24;
-  //   console.log(remainingHour);
-  let remainingMinute = Math.floor(timeToCX / 1000 / 60) % 60;
-  //   console.log(remainingMinute);
-  let remainingSecond = Math.floor(timeToCX / 1000) % 60;
-  //   console.log(remainingSecond);
-
-  days.innerHTML = remainingDay;
-  hours.innerHTML = addZero(remainingHour);
-  minutes.innerHTML = addZero(remainingMinute);
-  seconds.innerHTML = addZero(remainingSecond);
+    days.innerHTML = remainingDay;
+    hours.innerHTML = addZero(remainingHour);
+    minutes.innerHTML = addZero(remainingMinute);
+    seconds.innerHTML = addZero(remainingSecond);
+  }
 }
 
 // Set interval every 1000ms / 1 second
@@ -53,4 +60,3 @@ function addZero(time) {
 
   return result;
 }
-// console.log(addZero(9));
